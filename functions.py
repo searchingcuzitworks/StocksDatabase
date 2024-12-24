37c4af7fd4e768123c41e264ce9d0a5a4ca9536e
searchingcuzitworks <krtingupta259@gmail.com>
2024-12-24 14:45:40 +0000

    new file:   functions.py 	new file:   main_frame.py 	deleted:    streamlit_app.py

---
+++ functions.py
@@ -0,0 +1,17 @@
+# Modules
+import pandas as pd
+import json
+import requests
+from urllib.request import urlopen
+import certifi
+
+# Algrorithm - All stocks in NSE
+def nse_stocks():
+    url = "https://financialmodelingprep.com/api/v3/symbol/NSE?apikey=kOA329x0FUKk3nBlZAh3w9vFECMRkdi5"
+    response = urlopen(url, cafile=certifi.where())
+    data = response.read().decode("utf-8")
+    data = json.loads(data)
+    print(data)
+    df = pd.DataFrame(data)
+    df = df.drop(columns = ["earningsAnnouncement", "timestamp"])
+    return df
---
+++ main_frame.py
@@ -0,0 +1,14 @@
+# Modules
+import streamlit as st
+import pandas as pd
+import functions
+
+class App:
+    def __init__(self):
+        self.left_column, self.middle_column, self.right_column = st.columns(3)
+    #@st.cache_data
+    def nse_stocks(self):
+        st.dataframe(functions.nse_stocks())
+
+app = App()
+app.nse_stocks()
